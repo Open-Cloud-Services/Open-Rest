42f4d86bfe88ddf52a489c1eac4f42db2b19f292
Tammo0987 <tammo.steffens@googlemail.com>
2018-10-25 22:34:48 +0200

    Added java docs

--- src/main/java/app/open/software/rest/RestHandler.java
+++ src/main/java/app/open/software/rest/RestHandler.java
@@ -1,17 +1,31 @@
 package app.open.software.rest;
 
+import app.open.software.rest.handler.RequestHandler;
 import app.open.software.rest.route.Router;
 import io.netty.channel.*;
 import io.netty.handler.codec.http.*;
 
+/**
+ * Handler for netty to create and send the response
+ *
+ * @author Tammo0987
+ * @since 1.0
+ * @version 1.0
+ */
 public class RestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
+	/**
+	 * {@link Router} to direct to thr right {@link RequestHandler}
+	 */
 	private final Router router;
 
 	RestHandler(final Router router) {
 		this.router = router;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	protected void channelRead0(final ChannelHandlerContext ctx, final FullHttpRequest request) throws Exception {
 		ctx.writeAndFlush(this.router.createResponse(request)).addListener(ChannelFutureListener.CLOSE);
 	}
