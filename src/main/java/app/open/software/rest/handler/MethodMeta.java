440077f9a408093013bde9ebea245237f9c39138
Tammo0987 <tammo.steffens@googlemail.com>
2018-10-25 22:53:47 +0200

    Added java docs

--- src/main/java/app/open/software/rest/handler/MethodMeta.java
+++ src/main/java/app/open/software/rest/handler/MethodMeta.java
@@ -2,15 +2,35 @@ package app.open.software.rest.handler;
 
 import app.open.software.rest.method.HttpMethod;
 import java.lang.reflect.Method;
+import java.net.URI;
 
+/**
+ * Method entity to provide some information about an rest method handler
+ *
+ * @author Tammo0987
+ * @since 1.0
+ * @version 1.0
+ */
 public class MethodMeta {
 
+	/**
+	 * {@link URI} route to the method
+	 */
 	private final String route;
 
+	/**
+	 * Type of {@link HttpMethod}
+	 */
 	private final HttpMethod httpMethod;
 
+	/**
+	 * {@link RequestHandler} in which the method is located
+	 */
 	private final RequestHandler requestHandler;
 
+	/**
+	 * The {@link Method} himself
+	 */
 	private final Method method;
 
 	MethodMeta(final String route, final HttpMethod httpMethod, final RequestHandler requestHandler, final Method method) {
